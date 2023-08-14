56cab120570dc7f754a5d6a9601eed297da9d4ef
Ivan Henriquez <ivan.henriquez@omegaccs.com>
2023-08-14 18:23:58 -0400

    prueba api 2

--- src/components/BoxFact.tsx
+++ src/components/BoxFact.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 
 const BoxFact = () => {
-  const [data, setData] = useState({});
+  const [data, setData] = useState<any>({});
 
   useEffect(() => {
     generarDatos();
